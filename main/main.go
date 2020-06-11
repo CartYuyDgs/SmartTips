b518b5b5708b5deeded45b6d6a5bac109a6746f1
于洋 <yy_dgs@126.com>
2020-06-11 22:57:57 +0800

    首页

--- main/main.go
+++ main/main.go
@@ -1,7 +1,28 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func main() {
 	//加载学校数据
-
+	http.HandleFunc("/index", handlerIndex)
 	//接口
+	http.HandleFunc("/search", handlerSearch)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+}
+
+func handlerIndex(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello world!"))
+}
+
+func handlerSearch(w http.ResponseWriter, r *http.Request) {
+
 }
